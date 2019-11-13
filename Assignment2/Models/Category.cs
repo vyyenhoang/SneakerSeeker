63233f4579e7f0c42fac7502f1929bff6f214347
Linh Duong <200393657@student.georgianc.on.ca>
2019-11-13 18:11:50 -0500

    Category Model Update

--- Assignment2/Models/Category.cs
+++ Assignment2/Models/Category.cs
@@ -6,30 +6,33 @@ using System.Threading.Tasks;
 
 namespace Assignment2.Models
 {
-    public class Category
-    {
-        public Category() { }
-        public Category(int GenreId, String Name, String Description)
-        {
-            this.CategoryId = CategoryId;
-            this.Name = Name;
-            this.Description = Description;
-        }
+	public class Category
+	{
+		public Category() { }
+		public Category(int CategoryId, String CategoryName, String Description, Boolean Active)
+		{
+			this.CategoryId = CategoryId;
+			this.CategoryName = CategoryName;
+			this.Description = Description;
+			this.Active = Active
 
-        [Key]
-        public virtual int CategoryId { get; set; } //Primary Key
+		}
+
+		[Key]
+		public virtual int CategoryId { get; set; } //Primary Key
 
 
-        [Required]
-        [Display(Name = "Category Name")]
-        public virtual String Name { get; set; }
+		[Required]
+		[Display(Name = "Category Name")]
+		public virtual String CategoryName { get; set; }
 
-        [Required]
-        public virtual String Description { get; set; }
+		[Required]
+		public virtual String Description { get; set; }
 
-        public virtual String Picture { get; set; }
+		public virtual String ImageURL { get; set; }
 
-        public virtual Boolean Active { get; set; }
+		public virtual Boolean Active { get; set; }
 
+		public virtual List<Category> CateName { get; set; }
     }
 }
--- Assignment2/Models/Customers.cs
+++ Assignment2/Models/Customers.cs
@@ -6,39 +6,7 @@ using System.Threading.Tasks;
 
 namespace Assignment2.Models
 {
-    public class Customers
-    {
-        [Key]
-        public virtual int CustomerId { get; set; } //Primary Key
-
-        public virtual String FirstName { get; set; }
-        public virtual String LastName { get; set; }
-        public virtual String Class { get; set; }
-        public virtual String Room { get; set; }
-        public virtual String Building { get; set; }
-        public virtual String Address1 { get; set; }
-        public virtual String Address2 { get; set; }
-        public virtual String City { get; set; }
-        public virtual String State { get; set; }
-        public virtual String PostalCode { get; set; }
-        public virtual String Phone { get; set; }
-        public virtual String Email { get; set; }
-        public virtual String VoiceMail { get; set; }
-        public virtual String Password { get; set; }
-        public virtual String CreditCard { get; set; }
-        public virtual String CreditCardTypeId { get; set; }
-        public virtual String CardExpMo { get; set; }
-        public virtual String CardExpYr { get; set; }
-        public virtual String BillingAddress { get; set; }
-        public virtual String BillingCity { get; set; }
-        public virtual String BillingRegion { get; set; }
-        public virtual String BillingPostalCode { get; set; }
-        public virtual String BillingCountry { get; set; }
-        public virtual String ShipAddress { get; set; }
-        public virtual String ShipCity { get; set; }
-        public virtual String ShipRegion { get; set; }
-        public virtual String ShipPostalCode { get; set; }
-        public virtual String ShipCountry { get; set; }
-        public virtual String DateEntered { get; set; }
-    }
+	public class Customers { }
+	
 }
+
