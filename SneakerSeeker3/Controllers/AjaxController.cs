b2df5f489abf361abfd10d4abb55050b135db615
Vy <200408803@student.georgianc.on.ca>
2019-11-18 21:46:42 -0500

    Search Function Working
    search by SKU

---
+++ SneakerSeeker3/Controllers/AjaxController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SneakerSeeker3.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public IActionResult getPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ SneakerSeeker3/Controllers/SearchController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SneakerSeeker3.Data;
+using SneakerSeeker3.Models;
+
+namespace SneakerSeeker3.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public SearchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult Index(string query)
+        {
+            var products = GetProducts(query);
+            return PartialView(products);
+        }
+
+        public JsonResult autocomplete(string term)
+        {
+            var products = GetProducts(term);
+            String[] Results = products.Select(product => product.SKU).ToArray();
+            return new JsonResult(Results);
+        }
+
+        private List<Product> GetProducts(String query)
+        {
+            return _context
+                .Product
+                .Where(product => product.SKU
+                            .ToLower()
+                            .Contains(query.ToLower()))
+                .ToList();
+        }
+    }
+}
