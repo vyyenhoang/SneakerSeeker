70974d0aa91904029349824f1a51e2cd229e74ab
Linh Duong <200393657@student.georgianc.on.ca>
2019-12-05 22:29:15 -0500

    Add unit test

---
+++ SneakerSeekerTest/MockDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SneakerSeeker3.Data;
+using SneakerSeeker3.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SneakerSeekerTest
+{
+	public static class EntityExtensions
+	{
+		public static void Clear<T>(this DbSet<T> dbSet) where T : class
+		{
+			dbSet.RemoveRange(dbSet);
+		}
+	}
+	public class MockDb
+	{
+		public static ApplicationDbContext CreateMockDb()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>().
+			   UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+			using (var context = new ApplicationDbContext(options))
+			{
+				context.Users.Add(new SneakerSeekerUser { Email= "sneaker@test.com", Id = "sneaker@test.com", FirstName = "Seaker" , LastName = "Seeker"  });
+				context.ItemColor.Add(new ItemColor { ItemColorId = 1, Color = "Gold" });
+				context.ItemSize.Add(new ItemSize { ItemSizeId = 1, Size = "10" });
+				context.Category.Add (new Category {CategoryId = 1, CategoryName = "Sneakers", Description = "Sneakers for Women", Active = true });
+				context.Supplier.Add (new Supplier { SupplierId = 1, CompanyName = "Nike", URL = "https://i.pinimg.com/originals/4d/96/2d/4d962dee72fa76f023d411e20d30690c.jpg" });
+				context.Product.Add(new Product { ProductId = 1, SKU = "0001A", ProductName = "Nike Red Sneaker - Women", ProductDescription = "The lightest sneaker for daily life", CategoryId = 1, SupplierId = 1, ItemColorId = 1, UnitPrice = 200, ProductURL = "https://www.famousfootwear.com/ProductImages/shoes_ia92569.jpg?preset=details" });
+			}
+			return new ApplicationDbContext(options);
+		}
+	}
+}
---
+++ SneakerSeekerTest/ProductControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SneakerSeeker3.Controllers;
+using SneakerSeeker3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SneakerSeekerTest
+{
+	public class ProductControllerTest
+	{
+		[Fact]
+		public async void AddProductTest()
+		{
+			//Arrange
+			var db = MockDb.CreateMockDb();
+			var c = new ProductsManagerController(db);
+
+			var product = new Product { ProductId = 1, SKU = "0001A", ProductName = "Nike Red Sneaker - Women", ProductDescription = "The lightest sneaker for daily life", CategoryId = 1, SupplierId = 1, ItemColorId = 1, UnitPrice = 200, ProductURL = "https://www.famousfootwear.com/ProductImages/shoes_ia92569.jpg?preset=details" };
+			//Act
+			var r = await c.Create(product);
+			//Assert
+			var result = Assert.IsType<RedirectToActionResult>(r);
+			Assert.Equal("Index", result.ActionName);
+			Assert.Equal(1, db.Product.Where(x => x.SKU == product.SKU && x.ProductName == product.ProductName && x.ProductDescription == product.ProductDescription).Count());
+		}
+
+		[Fact]
+		public async void IndexTest()
+		{
+			//Arrange
+			var db = MockDb.CreateMockDb();
+			ProductsManagerController pc = new ProductsManagerController(db);
+			//Act
+			var r = await pc.Index();
+			//Assert
+			var result = Assert.IsType<ViewResult>(r);
+			var model = Assert.IsAssignableFrom<List<Product>>(result.ViewData.Model);
+			//Assert.Equal(1, model.Count());
+		}
+
+
+	}
+}
