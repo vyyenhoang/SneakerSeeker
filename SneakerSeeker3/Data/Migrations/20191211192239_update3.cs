bdc2d4750355373414dbdfedef152e0f987a36e3
Simarpreet Singh <200413865@student.georgianc.on.ca>
2019-12-11 15:41:46 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ SneakerSeeker3/Data/Migrations/20191211192239_update3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SneakerSeeker3.Data.Migrations
+{
+    public partial class update3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
